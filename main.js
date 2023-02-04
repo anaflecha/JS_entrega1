7cc9be58c65408259cd63e8814b29c63a1c72742
Anabella Flecha <anabellaflecha@gmail.com>
2023-02-04 11:52:58 +0100

    entrega 1 js

---
+++ main.js
@@ -0,0 +1,41 @@
+function Producto(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  
+  const producto1 = new Producto("1-Banderín personalizado", 500);
+  const producto2 = new Producto("2-Cartel bienvenida", 500);
+  const producto3 = new Producto("3-Kit golosinas", 800);
+  const producto4 = new Producto("4-Imprimible comestible para alfajores", 1000);
+  
+  var productos = [producto1, producto2, producto3, producto4];
+  
+  let comprar = prompt(" Gracias por decorar tu fiesta con nuestros diseños. ¿Quiere comprar? Indique si o no");
+  var subtotal = 0;
+   if (comprar === "si") {
+     while (comprar === "si") {
+         let todoslosProductos = productos.map(
+      (producto, index) => `Item: ${index + 1} - ${producto.nombre} - $ ${producto.precio}`
+     );
+  
+    let productoSeleccionadoIndex = parseInt(
+      prompt(`${todoslosProductos.join("\n")}\n\nIngresa el número del item para tu selección`)
+        ) - 1;
+  
+    if (productoSeleccionadoIndex >= 0 && productoSeleccionadoIndex < productos.length) {
+      let selectedProduct = productos[productoSeleccionadoIndex];
+      alert("Tienes el producto: " + selectedProduct.nombre);
+      subtotal += selectedProduct.precio;
+    } else {
+      alert("No tenemos ese producto.");
+    }
+  
+    comprar = prompt( " Tu carrito es de $: " + subtotal + " " +"¿Deseas agregar otro producto? Responde si o no");
+    }
+  var total = subtotal *1.21;
+   prompt ("El total de tu compra (IVA incluido) es de: $ " + total + "\n\n"+"Ingresa tu correo para enviar los datos de pago");
+   alert("Correo enviado. Muchas gracias por su compra");
+}
+else{
+    alert(" :( Esperamos que vuelva pronto a nuestra tienda.");
+}
