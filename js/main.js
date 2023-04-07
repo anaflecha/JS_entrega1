72ae34dbf5db8581de1f31831401ecb397046105
Anabella Flecha <anabellaflecha@gmail.com>
2023-04-07 12:45:39 +0200

    githubpages

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ function renderizarProductos() {
   }
 };
 function obtenerJSON() {
-  $.getJSON("/json/products.json", function (respuesta, estado) {
+  $.getJSON("json/products.json", function (respuesta, estado) {
     if (estado == "success") {
       products = respuesta;
       renderizarProductos();
